73d33306fde8e2f981e98c23225dccaf59bd7ce0
Abdul Wasiv H <113840728+Abdulwasiv@users.noreply.github.com>
2023-12-31 17:41:11 +0530

    Add files via upload

---
+++ final_project/router/auth_users.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+let books = require("./booksdb.js");
+const regd_users = express.Router();
+
+let users = [{
+  "username": "James Smith",
+  "password": "1234"
+}
+];
+
+const isValid = (username)=>{ 
+  const user = users.filter(user => user.username === username);
+  if(user.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const authenticatedUser = (username,password)=>{ 
+  const user = users.filter(user => user.username === username);
+  if (username === user[0].username && password === user[0].password) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//only registered users can login
+regd_users.post("/login", (req,res) => {
+  const username = req.query.username;
+  const password = req.query.password;
+ 
+  if(isValid(username) && authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({
+      data: username
+    }, 'access')
+    req.session.authorization = { accessToken}
+    req.session.user = username
+
+    return res.status(200).json({message: "User logged in"});
+  }
+  
+  return res.status(401).json({message: "Access Denied"});
+});
+
+// Add a book review
+regd_users.put("/auth/review/:isbn", (req, res) => {
+  // const isbn = req.params.isbn;
+  // const userReview = req.query.review;
+  // const user = req.session.user
+
+  // const bookToBeReviewed = Object.values(books).filter(book => book.isbn === isbn);
+  // const reviews = bookToBeReviewed[0].reviews;
+  // reviews.review = userReview;
+  // reviews.username = user;
+  // //Add review to session
+  // req.session.book = bookToBeReviewed;
+  
+  // return res.status(200).json({message: `Review: '${userReview}' was added by '${user}'`, book: bookToBeReviewed});
+
+  // Assuming isbn is extracted from the route parameters
+const isbn = req.params.isbn;
+
+// Retrieve review from the request body
+const userReview = req.body.review;
+
+// Check if user is defined in the session
+const user = req.session.user;
+
+
+const bookToBeReviewed = Object.values(books).filter(book => book.isbn === isbn);
+
+if (bookToBeReviewed.length === 0) {
+  return res.status(404).json({ message: "Book not found" });
+}
+
+const reviews = bookToBeReviewed[0].reviews;
+reviews.review = userReview;
+reviews.username = user;
+
+return res.status(200).json({
+  message: `Review: '${userReview}' was added by '${user}'`,
+  book: bookToBeReviewed
+});
+console.log("Review:", userReview);
+console.log("User:", user);
+
+});
+
+//Delete book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const bookToDeleteReview = Object.values(books).filter(book => book.isbn === isbn);
+
+  if(bookToDeleteReview.length > 0) {
+    if(req.session.user === bookToDeleteReview[0].reviews.username) {
+      delete bookToDeleteReview.reviews[0].review;
+      delete bookToDeleteReview.reviews[0].username;
+      return res.status(200).json({message: "success", book: bookToDeleteReview});
+    }
+  }
+  return res.status(200).json({message: "Review deleted", book: bookToDeleteReview});
+ // return res.status(403).json({message: "Access Denied to delete review", book: bookToDeleteReview});
+});
+
+module.exports.authenticated = regd_users;
+module.exports.isValid = isValid;
+module.exports.users = users;
---
+++ final_project/router/booksdb.js
@@ -0,0 +1,15 @@
+let books = {
+      "23021": {"isbn": "23021", "author": "Chinua Achebe", "title": "Things Fall Apart", "reviews": "good movie" },
+      "23024": {"isbn": "23024", "author": "Hans Christian Andersen", "title": "Fairy tales", "reviews": {}},
+      "23026": {"isbn": "23026", "author": "Dante Alighieri", "title": "The Divine Comedy", "reviews": {}},
+      "23028": {"isbn": "23028", "author": "Unknown", "title": "The Epic Of Gilgamesh", "reviews": {}},
+      "23020": {"isbn": "23020", "author": "Unknown", "title": "The Book Of Job", "reviews": {}},
+      "23012": {"isbn": "23012", "author": "Unknown", "title": "One Thousand and One Nights", "reviews": {}},
+      "23014": {"isbn": "23014", "author": "Unknown", "title": "Nj\u00e1l's Saga", "reviews": {}},
+      "23016": {"isbn": "23016", "author": "Jane Austen", "title": "Pride and Prejudice", "reviews": {}},
+      "23018": {"isbn": "23018", "author": "Honor\u00e9 de Balzac", "title": "Le P\u00e8re Goriot", "reviews": {}},
+      "23030": {"isbn": "23030", "author": "Samuel Beckett", "title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {}}
+    };
+    
+    module.exports = books;
+    
